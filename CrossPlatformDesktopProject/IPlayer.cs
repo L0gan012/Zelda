505d7c5325c1fab466f90231f4c0e7da86e87587
Logan Sauer <sauer.157@buckeyemail.osu.edu>
2020-05-29 14:13:26 -0400

    Added and editted existing interfaces

--- CrossPlatformDesktopProject/IPlayer.cs
+++ CrossPlatformDesktopProject/IPlayer.cs
@@ -9,5 +9,47 @@ namespace Sprint0
 
         //Draws the player
         void Draw(SpriteBatch spriteBatch, Texture2D texture);
+
+        //Damage player
+        void DamagePlayer();
+
+        //Use item
+        void UseItem();
+
+        //Idle left
+        void IdlePlayerLeft();
+
+        //Idle right
+        void IdlePlayerRight();
+
+        //Idle up
+        void IdlePlayerUp();
+
+        //Idle down
+        void IdlePlayerDown();
+
+        //Moves the player left
+        void MovePlayerLeft();
+
+        //Moves the player right
+        void MovePlayerRight();
+
+        //Moves the player up
+        void MovePlayerUp();
+
+        //Moves the player down
+        void MovePlayerDown();
+
+        //Player attacks left
+        void AttackLeft();
+
+        //Player attacks right
+        void AttackRight();
+
+        //Player attacks up
+        void AttackUp();
+
+        //Player attacks down
+        void AttackDown();
     }
 }
---
+++ CrossPlatformDesktopProject/IState.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint0
+{
+    interface IState
+    {
+        //Updates the State
+        void Update();
+
+        //Draws the sprite attached to the state
+        void Draw(SpriteBatch spriteBatch, Texture2D texture);
+    }
+}
