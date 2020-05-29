09211973cf23ba71460bf0e01ff6386daa9f51b7
Terryl Rumph <trumph15bx@yahoo.com>
2020-05-29 15:49:56 -0400

    Organizing

--- CrossPlatformDesktopProject/Game1.cs
+++ CrossPlatformDesktopProject/Game1.cs
@@ -15,10 +15,8 @@ namespace Sprint0
 
         private List<IController> controllers;
         private Dictionary<Keys, ICommand> keyboardCommands;
-        private Dictionary<Rectangle, ICommand> mouseCommands;
 
         private ISprite sprite;
-        private ISprite textSprite;
         
         //Game constructor
         public Game1()
@@ -35,28 +33,17 @@ namespace Sprint0
 
             //Make a dictionary of the keys and commands
             keyboardCommands = new Dictionary<Keys, ICommand>();
-
             keyboardCommands.Add(Keys.D0, new ExitCommand(this));
             keyboardCommands.Add(Keys.D1, new StationarySpriteCommand(this));
             keyboardCommands.Add(Keys.D2, new AnimatedSpriteCommand(this));
             keyboardCommands.Add(Keys.D3, new MovingSpriteCommand(this));
             keyboardCommands.Add(Keys.D4, new AnimatedMovingSpriteCommand(this));
 
-            //Make a dictionary of the quadrants and commands
-            mouseCommands = new Dictionary<Rectangle, ICommand>();
-
-            mouseCommands.Add(new Rectangle(), new ExitCommand(this));
-            mouseCommands.Add(new Rectangle(0, 0, this.GraphicsDevice.Viewport.Width/2, this.GraphicsDevice.Viewport.Height/2), new StationarySpriteCommand(this));
-            mouseCommands.Add(new Rectangle(this.GraphicsDevice.Viewport.Width/2, 0, this.GraphicsDevice.Viewport.Width / 2, this.GraphicsDevice.Viewport.Height / 2), new AnimatedSpriteCommand(this));
-            mouseCommands.Add(new Rectangle(0, this.GraphicsDevice.Viewport.Height/2, this.GraphicsDevice.Viewport.Width / 2, this.GraphicsDevice.Viewport.Height / 2), new MovingSpriteCommand(this));
-            mouseCommands.Add(new Rectangle(this.GraphicsDevice.Viewport.Width / 2, this.GraphicsDevice.Viewport.Height / 2, this.GraphicsDevice.Viewport.Width / 2, this.GraphicsDevice.Viewport.Height / 2), new AnimatedMovingSpriteCommand(this));
-
             //Make a list of controllers used
             controllers = new List<IController>();
 
             //Add a keyboard and a mouse controller
             controllers.Add(new KeyboardController(keyboardCommands));
-            controllers.Add(new MouseController(mouseCommands));
 
             base.Initialize();
         }
@@ -73,8 +60,6 @@ namespace Sprint0
 
             //Initialize the starting sprite to the stationary sprite
             sprite = new StationarySprite(this.GraphicsDevice.Viewport.Width, this.GraphicsDevice.Viewport.Height);
-            //Initialize the text sprite to TextSprite object
-            textSprite = new TextSprite(25, this.GraphicsDevice.Viewport.Height - 75, spriteFont, "Credits\nProgram Made By: Logan Sauer\nSprites from: https://www.spriters-resource.com/game_boy_gbc/pokemongoldsilver/sheet/9084/");
         }
 
         //Unloads content
