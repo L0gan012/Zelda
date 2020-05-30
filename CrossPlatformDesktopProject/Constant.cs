5fa5cc256d3888c51640e7e395136ae3b0c07c51
Barry Birschbach <birschbach.3@osu.edu>
2020-05-29 22:33:34 -0400

    Added Constant class and some of the constants needed for Sprite classes

---
+++ CrossPlatformDesktopProject/Constant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sprint0
+{
+    public class Class1
+    {
+        public static readonly int ScreenWidth = 800;
+        public static readonly int ScreenHeight = 480;
+        public static readonly Vector2 LinkStartPosition = new Vector2(200, 200);
+        public static readonly Vector2 EnemyStartPosition = new Vector2(600, 200);
+
+        //Animation Speed setting
+        public static readonly int TicksPerFrame = 8;
+
+        //One game grid square is 16*16 pixels.  DisplayScales multiply output size
+        public static readonly int DisplayScaleX = 3;
+        public static readonly int DisplayScaleY = 3;
+
+
+    }
+}
+
