34e791222b441ec698991e0d45f94cd6ec20cc2c
Logan Sauer <sauer.157@buckeyemail.osu.edu>
2020-06-04 11:51:32 -0400

    Added moving state classes

---
+++ CrossPlatformDesktopProject/Link States/MovingLinkLeftState.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Sprint2.Factory;
+using Sprint2.Player;
+using Sprint2.Sprite;
+
+namespace Sprint2
+{
+    class MovingLinkLeftState : ILinkState
+    {
+        //Instance variables
+        private Link link;
+        private ISprite sprite;
+
+        //Constructor with Link instance parameter
+        public MovingLinkLeftState(Link link)
+        {
+            this.link = link;
+            sprite = LinkSpriteFactory.Instance.CreateMovingLeftLinkGreenSprite(link.Position);
+        }
+
+        //Updates the State
+        public void Update()
+        {
+            sprite.Update();
+        }
+
+        //Draws the sprite attached to the state
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            sprite.Draw(spriteBatch);
+        }
+
+        //Damage Link
+        public void DamageLink() { }
+
+        //Use compass item
+        public void UseItem(IItem item)
+        {
+            item.Use();
+            link.State = new UseItemLeftState(link);
+        }
+
+        //Idle Link left
+        public void SetLinkIdle()
+        {
+            link.State = new IdleLinkLeftState(link);
+        }
+
+        //Moves Link left
+        public void MoveLinkLeft()
+        {
+            //Already moving left
+        }
+
+        //Moves Link right
+        public void MoveLinkRight()
+        {
+            link.State = new MovingLinkRightState(link);
+        }
+
+        //Moves Link up
+        public void MoveLinkUp()
+        {
+            link.State = new MovingLinkUpState(link);
+        }
+
+        //Moves Link down
+        public void MoveLinkDown()
+        {
+            link.State = new MovingLinkDownState(link);
+        }
+
+        //Link attacks
+        public void Attack()
+        {
+            link.State = new AttackingLinkLeftState(link);
+        }
+    }
+}
