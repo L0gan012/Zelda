5aeba956f403e8162896d2bb9045cde7eb311f61
Barry Birschbach <birschbach.3@osu.edu>
2020-06-07 13:36:05 -0400

    Fixed Link Sword after effects missing on down swing

--- CrossPlatformDesktopProject/Sprite/SpriteLinkAttackWoodenSwordDownGreen.cs
+++ CrossPlatformDesktopProject/Sprite/SpriteLinkAttackWoodenSwordDownGreen.cs
@@ -60,8 +60,8 @@ namespace Sprint2.Sprite
                 sourceRectangleAvatar = new Rectangle(192, 16, 16, 16);
                 destinationRectangleAvatar = new Rectangle((int)position.X, (int)position.Y, 16 * Constant.DisplayScaleX, 16 * Constant.DisplayScaleY);
 
-              //  sourceRectangleWeapon = new Rectangle(192 + (int)weaponOffset.X, 16 + (int)weaponOffset.Y, 8, 11);
-              //  destinationRectangleWeapon = new Rectangle((int)position.X + (int)weaponOffset.X * Constant.DisplayScaleX, (int)position.Y + (int)weaponOffset.Y * Constant.DisplayScaleY, 8 * Constant.DisplayScaleX, 11 * Constant.DisplayScaleY);
+                sourceRectangleWeapon = new Rectangle(192 + (int)weaponOffset.X, 16 + (int)weaponOffset.Y, 8, 11);
+                destinationRectangleWeapon = new Rectangle((int)position.X + (int)weaponOffset.X * Constant.DisplayScaleX, (int)position.Y + (int)weaponOffset.Y * Constant.DisplayScaleY, 8 * Constant.DisplayScaleX, 11 * Constant.DisplayScaleY);
             }
             else if (currentFrame == 1)
             {
@@ -91,7 +91,7 @@ namespace Sprint2.Sprite
 
             spriteBatch.Begin();
             spriteBatch.Draw(texture, destinationRectangleAvatar, sourceRectangleAvatar, color);
-          //  spriteBatch.Draw(texture, destinationRectangleWeapon, sourceRectangleWeapon, color);
+            spriteBatch.Draw(texture, destinationRectangleWeapon, sourceRectangleWeapon, color);
             spriteBatch.End();
 
 
